49f8b8417a173ff9da4de9857e0c5a325d3ed148
cgraf <carson.graf@utexas.edu>
2023-10-27 15:22:46 -0500

    saved code

--- MSP432/LS20031/point_engine.h
+++ MSP432/LS20031/point_engine.h
@@ -8,6 +8,18 @@
 #ifndef POINT_ENGINE_H_
 #define POINT_ENGINE_H_
 
+#define num_points 256
 
+typedef struct point{
+    float latitude;
+    float longitude;
+} point;
+
+typedef struct path{
+    point cur_point;
+    point[num_points] past_point;
+    uint16_t idx;
+    uint32_t time_stamp;
+} path;
 
 #endif /* POINT_ENGINE_H_ */
